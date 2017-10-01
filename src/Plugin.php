a520d61b68f56763651237716ba805f586f3f147
basemkhirat <basemkhirat@gmail.com>
2017-10-01 18:17:38 +0200

    fixes

--- src/Plugin.php
+++ src/Plugin.php
@@ -13,9 +13,11 @@ class Plugin extends \Dot\Platform\Plugin
     {
 
         Navigation::menu("sidebar", function ($menu) {
+
             if (Auth::user()->hasRole('superadmin')) {
                 $menu->item('users.permissions', trans("admin::common.permissions"), URL::to(ADMIN . '/roles'));
             }
+
         });
 
         include __DIR__ . "/routes.php";
