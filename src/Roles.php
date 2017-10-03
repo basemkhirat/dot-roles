a5910f8558c6933a6bbe455ae0fb4773e5b0852b
basemkhirat <basemkhirat@gmail.com>
2017-10-03 08:27:41 +0200

    extend

--- src/Roles.php
+++ src/Roles.php
@@ -6,21 +6,18 @@ use Auth;
 use Navigation;
 use URL;
 
-class Plugin extends \Dot\Platform\Plugin
+class Roles extends \Dot\Platform\Plugin
 {
 
     function boot()
     {
 
-        Navigation::menu("sidebar", function ($menu) {
+        parent::boot();
 
+        Navigation::menu("sidebar", function ($menu) {
             if (Auth::user()->hasRole('superadmin')) {
                 $menu->item('users.permissions', trans("admin::common.permissions"), URL::to(ADMIN . '/roles'));
             }
-
         });
-
-        include __DIR__ . "/routes.php";
-
     }
 }
