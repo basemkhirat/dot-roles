250a260266601381e37a1fac94d7327baf3175a5
basemkhirat <basemkhirat@gmail.com>
2017-10-06 18:23:30 +0200

    fixes

--- src/routes.php
+++ src/routes.php
@@ -1,13 +1,14 @@
 <?php
 
-Route::group(array(
+Route::group([
     "prefix" => ADMIN,
-    "middleware" => ["web", "auth", "can:roles.manage"],
-        ), function($route) {
-    $route->group(array("prefix" => "roles"), function($route) {
-        $route->any('/', array("as" => "admin.roles.show", "uses" => "Dot\Roles\Controllers\RolesController@index"));
-        $route->any('/create', array("as" => "admin.roles.create", "uses" => "Dot\Roles\Controllers\RolesController@create"));
-        $route->any('/{id}/edit', array("as" => "admin.roles.edit", "uses" => "Dot\Roles\Controllers\RolesController@edit"));
-        $route->any('/delete', array("as" => "admin.roles.delete", "uses" => "Dot\Roles\Controllers\RolesController@delete"));
+    "middleware" => ["web", "auth:backend", "can:roles.manage"],
+    "namespace" => "Dot\\Roles\\Controllers"
+], function ($route) {
+    $route->group(["prefix" => "roles"], function ($route) {
+        $route->any('/', ["as" => "admin.roles.show", "uses" => "RolesController@index"]);
+        $route->any('/create', ["as" => "admin.roles.create", "uses" => "RolesController@create"]);
+        $route->any('/{id}/edit', ["as" => "admin.roles.edit", "uses" => "RolesController@edit"]);
+        $route->any('/delete', ["as" => "admin.roles.delete", "uses" => "RolesController@delete"]);
     });
 });
