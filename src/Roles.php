06600436812f08871462bef47de4c0af2eabd6b6
basem <basemkhirat@gmail.com>
2019-12-16 19:17:31 +0200

    version 0.5

--- src/Roles.php
+++ src/Roles.php
@@ -21,16 +21,18 @@ class Roles extends \Dot\Platform\Plugin
 
         // Roles for superadmins only
 
-        $this->gate->define("roles.manage", function ($user) {
-            return $user->hasRole("superadmin");
+        $this->gate->define("roles.manage", function ($user, $profile = false) {
+            if($profile){
+                return $user->hasRole("superadmin") and $user->id != $profile->id;
+            }else{
+                return $user->hasRole("superadmin");
+            }
         });
 
         Navigation::menu("sidebar", function ($menu) {
-
             if (Auth::user()->hasRole('superadmin')) {
                 $menu->item('permissions', trans("admin::common.permissions"), route("admin.roles.show"))->icon("fa-unlock-alt")->order(17);
             }
-
         });
     }
 }
